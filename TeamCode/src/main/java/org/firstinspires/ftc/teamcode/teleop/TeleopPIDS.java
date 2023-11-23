a3aeca9190944b27febfe23c2a4c35e74514364c
skandacode <39654761+skandacode@users.noreply.github.com>
2023-11-23 09:31:47 -0600

    add half open

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopPIDS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopPIDS.java
@@ -10,11 +10,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
 
+import java.lang.annotation.ElementType;
 import java.util.List;
 @TeleOp
 @Config
@@ -51,6 +53,8 @@ public class TeleopPIDS extends LinearOpMode {
         waitForStart();
         double loopTime=0.0;
         lift.resetEncoder();
+        ElapsedTime timer=new ElapsedTime();
+        boolean prev_open=false;
 
         while (opModeIsActive()) {
             hubs.forEach(LynxModule::clearBulkCache);
@@ -88,12 +92,21 @@ public class TeleopPIDS extends LinearOpMode {
                         telemetry.addLine("going down");
                 }
             }
-
-            if (gamepad2.right_bumper && (gamepad2.right_trigger>0.3|| gamepad2.x)){
-                lift.open();
+            boolean current_open=gamepad2.right_bumper && (gamepad2.right_trigger>0.3|| gamepad2.x);
+            if (current_open){
+                if (!prev_open) {//first time it is opened
+                    timer.reset();
+                    lift.half_open();
+                }else if (timer.milliseconds()<500){
+                    lift.half_open();
+                }else{
+                    lift.open();
+                }
             }else{
                 lift.close();
             }
+            prev_open=current_open;
+
             if (gamepad2.dpad_down){hangservo.setPosition(1);}//down
             if (gamepad2.dpad_right){hangservo.setPosition(0);}//up
             if (gamepad2.dpad_up){hangservo.setPosition(0.25);}//locks before hang
